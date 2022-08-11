9ecba7d4cc1fb56b37034357506b6e8e0f375334
e-goncalves <egoncalves@tecgraf.puc-rio.br>
2022-08-11 11:36:33 -0300

    Criação de Endpoint - READ ALL [GET]/pontuacoes

--- backend/index.js
+++ backend/index.js
@@ -9,5 +9,30 @@ app.get("/oi", function (req, res) {
     res.send("Olá, mundo!");
 });
 
+//Criar lista com as pontuações
+const lista = [
+    {
+        id: 1,
+        nome: "Paulo",
+        pontos: 90
+    },
+    {
+        id: 2,
+        nome: "Daniel",
+        pontos: 52
+    },
+    {
+        id: 3,
+        nome: "Bia",
+        pontos: 97
+    }
+];
+//Endpoint READ ALL - [GET] /pontuacoes
+app.get("/pontuacoes", function (req, res){
+    res.send("Ler todas as pontuações");
+});
+//Endpoint CREATE - [POST] /pontuacoes
+//Endpoint CREATE - [POST] /pontuacoes
+
 app.listen(3000);
 //console.log("Executando no sevidor localhost:3000");
